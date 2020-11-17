2858af422b25a53e921cd61216e0d244a46011c2
WandaRMaldo <wanda.rocha.maldonado@gmail.com>
2020-11-17 18:26:07 -0300

    Updates

---
+++ precache-manifest.988a28628b6b05923149f59f166a4d72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da2419d2cffb4f546b0bbeb54c7736ec",
+    "url": "/tarea_Hooks/index.html"
+  },
+  {
+    "revision": "263c6e60359e93453cea",
+    "url": "/tarea_Hooks/static/css/main.1d2304c5.chunk.css"
+  },
+  {
+    "revision": "0aa2de919c0e874c1c7f",
+    "url": "/tarea_Hooks/static/js/2.3d3581aa.chunk.js"
+  },
+  {
+    "revision": "63578bd18f7d62dbc9a1cb42d77740d7",
+    "url": "/tarea_Hooks/static/js/2.3d3581aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "263c6e60359e93453cea",
+    "url": "/tarea_Hooks/static/js/main.0ea468c4.chunk.js"
+  },
+  {
+    "revision": "6fd362ab680621ddaee2",
+    "url": "/tarea_Hooks/static/js/runtime-main.1509c837.js"
+  }
+]);
